e0a4efb65c3660eb60eede22c32abd672dfdd457
prasetyo putranto <prasetyo531@gmail.com>
2025-02-12 12:00:29 +0700

    handle case not found country

--- 8.3-travel-tracker/solution1.js
+++ 8.3-travel-tracker/solution1.js
@@ -17,28 +17,43 @@ db.connect();
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
+async function checkVisisted() {
+  const result = await db.query("SELECT country_code FROM visited_countries");
+
+  let countries = [];
+  result.rows.forEach((c) => {
+    countries.push(c.country_code);
+  });
+  console.log('function check visited '+countries);
+  return countries;
+}
+
 // GET home page
-app.get("/", async (req, res) => {
+app.get("/", async (_req, res) => {
   const result = await db.query("SELECT country_code FROM visited_countries");
   let country = [];
   result.rows.forEach((c) => {
     country.push(c.country_code);
   });
-  console.log(result.rows);
+  console.log('get case '+result.rows);
+  // res.redirect method sends a response to the client instructing it to redirect to a different URL
+  // res.render method renders a template with the given data and sends the rendered HTML to the client
   // first key is always from ejs file
   res.render("index.ejs", { countries: country, total: country.length });
-  db.end();
 });
 
+//INSERT new country
 app.post("/add", async (req, res) => {
   const input = req.body["country"];
 
-  const result = await db.query(
-    "SELECT country_code FROM countries WHERE country_name = $1",
-    [input]
-  );
+  //first try, catch error TypeError: Cannot read properties of undefined (reading 'country_code')
+  try {
+    console.log(input);
+    const result = await db.query(
+      "SELECT country_code FROM countries WHERE country_name = $1",
+      [input]
+    );
 
-  if (result.rows.length !== 0) {
     const data = result.rows[0];
     const countryCode = data.country_code;
 
@@ -46,6 +61,14 @@ app.post("/add", async (req, res) => {
       countryCode,
     ]);
     res.redirect("/");
+  } catch (error) {
+    console.log(error);
+    const countries = await checkVisisted();
+    res.render("index.ejs", {
+      countries: countries,
+      total: countries.length,
+      error: "Country name does not exist, try again.",
+    });
   }
 });
 
